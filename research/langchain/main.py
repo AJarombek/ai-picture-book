7e28bc34a0227b5c2f2bb02c345f1cc9b7f052cd
ajarombek <ajarombek95@gmail.com>
2023-11-06 11:53:19 -0500

    basic chatbot

--- research/langchain/main.py
+++ research/langchain/main.py
@@ -1,6 +1,10 @@
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import SystemMessage, HumanMessage, BaseMessage
 
+HELLO = "Bonjour!  Je m'appelle Andy.  Je suis un professeur de français."
+EXIT = "sortie"
+BYE = "Salut!"
+
 if __name__ == '__main__':
     # Requires an OpenAI API key in the 'OPENAI_API_KEY' environment variable to run
     # API requires a paid subscription
@@ -9,7 +13,20 @@ if __name__ == '__main__':
     messages = [
         SystemMessage(content="You are a kind, charismatic french instructor."),
         SystemMessage(content="You answer in French but clarify in English so that beginner students can understand."),
-        HumanMessage(content="How do I say 'I'm sorry, I don't speak French very well' in French?"),
+        SystemMessage(content="Your responses should feel like a natural conversation with another human."),
     ]
-    response: BaseMessage = chat(messages)
-    print(response.content)
+
+    print("Français Chatbot v1.0.0")
+    print(HELLO)
+
+    while True:
+        user_input = input("> ")
+
+        if user_input == EXIT:
+            print(BYE)
+            break
+        else:
+            messages.append(HumanMessage(content=user_input))
+            response: BaseMessage = chat(messages)
+            print(response.content)
+            messages.append(response)
