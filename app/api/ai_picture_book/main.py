149d2e4215bd94728457283505da85d345721589
ajarombek <ajarombek95@gmail.com>
2023-12-06 13:16:16 -0500

    api & ui integrated

--- app/api/ai_picture_book/main.py
+++ app/api/ai_picture_book/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["http://localhost:8080"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 async def generate_response_stream(user_input: str):
