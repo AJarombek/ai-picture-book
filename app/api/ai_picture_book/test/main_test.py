efca6909d9e0cdd49e2c35abc85ec2b0d49b8ae3
ajarombek <ajarombek95@gmail.com>
2023-12-15 15:10:53 -0500

    add test code

--- app/api/ai_picture_book/test/main_test.py
+++ app/api/ai_picture_book/test/main_test.py
@@ -25,3 +25,30 @@ def test_chat_endpoint_empty_input(client: TestClient):
     response = client.get("/echo?user_input=")
     assert response.status_code == 400
     assert response.json() == {"detail": "user_input cannot be empty"}
+
+
+def test_chat_endpoint_no_input(client: TestClient):
+    response = client.get("/echo")
+    assert response.status_code == 422
+    assert response.json() == {
+        "detail": [
+            {
+                "input": None,
+                "loc": ["query", "user_input"],
+                "msg": "Field required",
+                "type": "missing",
+                "url": "https://errors.pydantic.dev/2.5/v/missing",
+            }
+        ]
+    }
+
+
+def test_generate_endpoint_mock(client: TestClient, monkeypatch):
+    def mock_openai_response_stream(user_input: str):
+        yield f"data: {user_input} from open ai\n\n"
+
+    monkeypatch.setattr("ai_picture_book.main.openai_response_stream", mock_openai_response_stream)
+    sample_user_input = "Hello"
+    response = client.get(f"/generate?user_input={sample_user_input}")
+    assert response.status_code == 200
+    assert response.text == 'data: Hello from open ai\n\n'
