99030aaeb1c739d43193152b44cbe058a77fbacd
Andrew Jarombek <andrew@jarombek.com>
2023-12-29 10:03:43 -0500

    Server-Sent Event Integration for Main Page, Ping Page (#15)
    * ping page, echo endpoint for main page
    
    * go router

--- app/ui/picture_book/lib/main.dart
+++ app/ui/picture_book/lib/main.dart
@@ -1,43 +1,74 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:picture_book/ping.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
+final _router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const Home(title: 'AI Picture Book'),
+    ),
+    GoRoute(
+      path: '/ping',
+      builder: (context, state) => const Ping(),
+    ),
+  ],
+);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'AI Picture Book',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'AI Picture Book'),
+      routerConfig: _router,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class Home extends StatefulWidget {
+  const Home({super.key, required this.title});
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<Home> createState() => _HomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  Future<String> fetchData() async {
-    final response = await http.get(Uri.parse('http://localhost:8000/'));
+class _HomeState extends State<Home> {
+  final TextEditingController _controller = TextEditingController();
+  String _responseText = '';
+  bool _isLoading = false;
+
+  void _sendDataToServer() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    final response = await http.get(
+      Uri.parse('http://localhost:8000/generate?user_input=${_controller.text}'),
+    );
+
     if (response.statusCode == 200) {
-      return response.body;
+      setState(() {
+        _responseText = response.body;
+        _isLoading = false;
+      });
     } else {
-      throw Exception('Failed to load data');
+      setState(() {
+        _responseText = 'Error: ${response.statusCode}';
+        _isLoading = false;
+      });
     }
   }
 
@@ -48,18 +79,36 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: FutureBuilder<String>(
-          future: fetchData(),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              final data = json.decode(snapshot.data!);
-              return Text('Data: $data');
-            } else if (snapshot.hasError) {
-              return Text('${snapshot.error}');
-            }
-            return const CircularProgressIndicator();
-          }
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            TextField(
+              controller: _controller,
+              decoration: const InputDecoration(
+                labelText: 'Enter Text',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: _isLoading ? null : _sendDataToServer,
+              child: const Text('Send'),
+            ),
+            const SizedBox(height: 20.0),
+            _isLoading ?
+            SpinKitCircle(
+              color: Theme.of(context).colorScheme.primary,
+              size: 50.0,
+            )
+            :
+            Expanded(
+              child: Center(
+                child: Text(_responseText),
+              ),
+            ),
+          ],
         ),
       ),
     );
